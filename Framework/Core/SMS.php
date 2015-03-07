103055a7e8308f6ac99212df3d2ae66a2d81a6fa
Mehmet Nuri Öztürk <mnozturk@outlook.com.tr>
2015-03-07 10:47:06 +0200

    SMS Class is updated and added report service

--- Framework/Core/SMS.php
+++ Framework/Core/SMS.php
@@ -1,26 +1,36 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Mehmet Nuri Öztürk
- * Date: 3/6/15
- * Time: 4:23 PM
+ * This class is send sms for your members
+ * @author Mehmet Nuri Öztürk <info@mehmetnuriozturk.com.tr>
+ * @copyright Copyright 2015, UPSF Corp.
+ * @version 1.0
+ * @since 2015
  */
 
-
-class SMS extends Text
+class SasMS extends Text
 {
-    public function Send($username,$password, $message, $mobileno, $message_header)
+    public  function startdateconvert($startdate)
     {
-
         $startdate=date('d.m.Y H:i');
-        $startdate=str_replace('.', '',$startdate );
+        $startdate=str_replace('.', '',$startdate);
         $startdate=str_replace(':', '',$startdate);
         $startdate=str_replace(' ', '',$startdate);
 
+        return $startdate;
+    }
+
+    public function stopdateconvert($stopdate){
         $stopdate=date('d.m.Y H:i', strtotime('+1 day'));
         $stopdate=str_replace('.', '',$stopdate );
         $stopdate=str_replace(':', '',$stopdate);
         $stopdate = str_replace(' ', '', $stopdate);
+
+        return $stopdate;
+    }
+    public function Send($username,$password, $message, $mobileno, $message_header, $startdate, $stopdate)
+    {
+        $startdate = $this->startdateconvert($startdate);
+        $stopdate = $this->stopdateconvert($stopdate);
         $message = $this->convert($message, 'tr', 'en');
 
 
@@ -33,6 +43,24 @@ class SMS extends Text
         $output=curl_exec($curl_operation);
         curl_close($curl_operation);
         return $output;
+
+    }
+
+    public  function Report($username, $password, $bulkid, $mobilenumber, $type, $status, $ver, $startdate, $stopdate)
+    {
+        $startdate = $this->startdateconvert($startdate);
+        $stopdate = $this->stopdateconvert($stopdate);
+
+        $service_url = "http://api.netgsm.com.tr/httpbulkrapor.asp?usercode=$username&password=$password&bulkid=$bulkid&type=$type&status=$status&versiyon=$ver";
+
+        $crl_operation = curl_init();
+        curl_setopt($crl_operation, CURLOPT_URL, $service_url);
+        curl_setopt($crl_operation, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($crl_operation);
+        curl_close($crl_operation);
+        return $output;
+
+
 
     }
 
